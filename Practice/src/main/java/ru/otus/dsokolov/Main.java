b86ea1fb052ef6bfba7965af4abac863daf27334
theGreff <theGreff@mail.ru>
2021-12-15 07:56:44 +0300

    Домашняя работа 5. домены

--- Practice/src/main/java/ru/otus/dsokolov/Main.java
+++ Practice/src/main/java/ru/otus/dsokolov/Main.java
@@ -1,17 +1,20 @@
 package ru.otus.dsokolov;
 
+import org.h2.tools.Console;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import ru.otus.dsokolov.service.TestResultsProcessService;
+
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         ApplicationContext ctx = SpringApplication.run(Main.class, args);
-        TestResultsProcessService testResultsProcessService = ctx.getBean(TestResultsProcessService.class);
 
-        testResultsProcessService.runTest();
+        //PersonDao dao = context.getBean(PersonDao.class);
+
+        Console.main(args);
     }
 }
